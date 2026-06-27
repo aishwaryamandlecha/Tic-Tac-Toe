a51192aefb6f8520ef5be875743340989a576200
aishwaryamandlecha <aishwaryamandlecha13@gmail.com>
2026-06-27 13:26:47 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,114 @@
+// name = "tony stark";
+// x= null;
+// y=undefined;
+// age = 19;
+// price = 99.099;
+// isbool = false;
+
+// console.log(name);
+// let a;
+// console.log(a);
+// a=20;
+// console.log(a);
+// let big = BigInt("789");
+// console.log(big);
+// let sym = Symbol("hello world");
+// console.log(sym);
+// const student = {
+//     myName : "Aishu",
+//     myAge : 19,
+//     cgpa : 8.6 ,
+//     isPass : true ,
+// };
+// // to change 
+// student["myName" ] = "aishu mandlecha";
+// console.log(student );
+// console.log(student.myName);
+
+// console.log(student["myAge"] );
+// let name = prompt("name");
+// console.log(name);
+
+// for-of loop
+//  let str = "Aishu Mandlecha ";
+// for( let val of str ){
+//     console.log("val = ", val);
+// }
+
+// const student = {
+//     myName : "Aishu",
+//     myAge : 19,
+//     cgpa : 8.6 ,
+//     isPass : true ,
+// };
+
+// for( let key in student ){
+//     console.log("key = ", key, "value = ",student[key]);
+// }
+
+// template literal
+// let special = `THIS IS TEMPLATE LITERAL`;
+// console.log(special);
+// console.log(typeof special)
+
+// // usage 
+// let obj = {
+//     item : "pen  " ,
+//     price : 20,
+// };
+// let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+// // lec 5 
+// //arrow fnc
+// const arrowmal = (a,b) => {
+//     console.log(a*b);
+// };
+// arrowmal(345,65);
+
+// for each method
+//let arr = [0,1,2,3,4432,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// arr.forEach((val,idx) =>
+// {
+//     console.log((val*val),idx)
+// });
+
+//map
+
+// let newarr = arr.map((val) =>
+// {
+//     return (val*val);
+// });
+// console.log(newarr);
+
+// const output = arr.reduce((res , cur ) => {
+//     return res+cur;
+// });
+// console.log(output);
+// const output = arr.reduce((res , cur ) => {
+//     return res>cur ? res : cur;
+// });
+// console.log(output);
+// alert("its me bro again!!");
+// console.log("aishu");
+// console.dir(document.body);
+// let heading = document.getElementById("heading");
+// console.log('asihu');
+// console.dir(heading);
+
+// let ele = document.querySelectorAll("p");
+// console.dir(ele);
+
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + " from Apna College students";
+ 
+
+
+
+// console.log("Welcome to Aishu's clg");
+
+
+// console.log("mrrrrrrrrrrrrrrrrr");
+ 
